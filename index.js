fd1fa91b98061d9fe95097355390109f41aeadca
Gary McLean Hall <gary@igirisu.co>
2017-02-18 14:42:37 +0000

    Tried adding a layer of abstraction on the funcy setup

--- index.js
+++ index.js
@@ -5,43 +5,54 @@ const
       fun = require('funcy'),
       _ = require('highland')
 
+let matcher = (pattern, handler) => {
+  return [pattern, handler];
+}
+
+let route = (path, handler) => {
+  return matcher([path, fun.wildcard, fun.parameter], handler);
+}
+
+let catchall = (handler) => {
+  return route(fun.wildcard, handler)
+}
+
+let router = fun(
+  route('/', (response) => { 
+    return [ response, { status: 200, body: 'Hello World!' } ] 
+  }),
+  catchall((response) => { 
+    return [ response, { status: 400 } ] 
+  })
+)
+
+let responder = fun(
+  matcher([fun.parameter, {status: fun.parameter, body: fun.parameter}], 
+    (response, status, body) => {
+      response.status(status).send(body)
+    }
+  ),
+  matcher([fun.parmaeter, {status: fun.parameter}],
+    (response, status) => {
+      response.sendStatus(status)
+    }
+  )
+)
+
 let app = express()
 
 let requests = (push, next) => {
   app.get(/$/, (request, response) => {
     push(undefined, [ request.path, request, response ])
     next()
-  });
+  })
 }
 
-let routes = fun(
-  [
-    [ '/', fun.wildcard, fun.parameter ], 
-    (response) => { return [ response, { status: 200, body: 'Hello World!' } ] }
-  ],
-  [
-    [ fun.wildcard, fun.wildcard, fun.parameter ], 
-    (response) => { return [ response, { status: 400 } ] }
-  ]
-)
-
-let responder = fun(
-  [
-    [fun.parameter, {status: fun.parameter, body: fun.parameter}], 
-    (response, status, body) => response.status(status).send(body)
-  ],
-  [
-    [fun.parameter, {status: fun.parameter}], 
-    (response, status) => response.sendStatus(status)
-  ]
-)
-
-
 let server = app.listen(3000, () => {
-  console.log(`Listening on port ${server.address().port}`);
+  console.log(`Listening on port ${server.address().port}`)
 
   _(requests)
-  .map(routes)
+  .map(router)
   .each(responder)
 
 })
